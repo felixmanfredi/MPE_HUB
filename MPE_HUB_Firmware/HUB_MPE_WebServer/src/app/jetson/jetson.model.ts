a245e9831b628ab32904682718547985c1a6de58
Rocco <rdepietra@live.it>
2025-11-27 12:56:43 +0100

    Update 27/11/2025

--- MPE_HUB_Firmware/HUB_MPE_WebServer/src/app/jetson/jetson.model.ts
+++
@@ -1,8 +0,0 @@
-export interface JetsonData {
-    tensione: number;
-    corrente: number;
-    potenza: number;
-    state: string;
-    isPowered: boolean;
-    isConnected: boolean;
-  }
--- MPE_HUB_Firmware/HUB_MPE_WebServer/src/app/videoenc/videoenc.model.ts
+++
@@ -1,8 +0,0 @@
-export interface VideoencData {
-    tensione: number;
-    corrente: number;
-    potenza: number;
-    state: string;
-    isPowered: boolean;
-    isConnected: boolean;
-  }
